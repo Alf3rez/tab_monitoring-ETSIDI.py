acb1552c1b8fcb6873c7f87fcca7dd4fb1e80af1
Alfred0 <90577614+Alf3rez@users.noreply.github.com>
2021-09-16 11:59:47 +0200

    Update Monitorizacion.py

--- Monitorizacion.py
+++ Monitorizacion.py
@@ -5,45 +5,63 @@
 import PySimpleGUI as sg
 import psutil
 import time
+import GPUtil
+import platform
+
 
 sg.theme('BrownBlue') #Para elegir el tema de la ventana
 
-
-
     #Variables CPU
 
-CPUus = psutil.cpu_percent(interval=0.5)
-CPUfq = psutil.cpu_freq(percpu=False)
+CPUus = psutil.cpu_percent(interval=1)
+CPUfq = psutil.cpu_freq(percpu=False).current
 
     #Variables RAM
 
-RAMus = psutil.swap_memory()
+RAMus = psutil.swap_memory().percent
+RAMused = psutil.swap_memory().used
+RAMfree = psutil.swap_memory().free
 
     #Variables GPU
+"""
+GPUs = GPUtil.getGPUs()
 
+gpu_load = f"{gpu.load*100}"
+gpu_used_memory = f"{gpu.memoryUsed}"
+
+GPUus = gpu_load
+GPUmem = gpu_used_memory
+"""
 
     #Variables Discos
 
-DISKus = psutil.disk_usage('/')
+DISKus = psutil.disk_usage('/').percent
+Diskload = psutil.disk_usage('/').used
+Diskfree = psutil.disk_usage('/').free
 
 #Pestañas
 
 
-tab1_layout =  [[sg.Text(f'CPU uso: {CPUus} %')], 
-                [sg.Text(f'CPU frecuencia: {CPUfq} %')]
+tab1_layout =  [[sg.Text(f'CPU carga: {CPUus} %')], 
+                [sg.Text(f'CPU frecuencia: {CPUfq*0.001} MHz')]
                ]
 
-tab2_layout =  [[sg.Text(f'RAM uso: {RAMus} %')]]
+tab2_layout =  [[sg.Text(f'RAM carga: {RAMus} %')],
+                [sg.Text(f'RAM utilizada: {RAMused*0.0000000009313225746:.2f} GB')],
+                [sg.Text(f'RAM libre: {RAMfree*0.0000000009313225746:.2f} GB')]]
+                
+tab3_layout =  [[sg.Text(f'GPU uso: {CPUus} %')], #Esta con los valores de la CPU para poder seguir usando el código sin tener que poner 4 o 5 comentarios mas mientras averiguo como sacar datos de la GPU
+                [sg.Text(f'GPU memoria: {CPUus} MB')]]
 
-tab3_layout =  [[sg.Text('GPU uso: {} %')]]
-
-tab4_layout =  [[sg.Text(f'Disco uso: {DISKus} %')]]
+tab4_layout =  [[sg.Text(f'Disco carga: {DISKus} %')],
+                [sg.Text(f'Disco utilizado: {Diskload*0.0000000009313225746:.2f} GB')],
+                [sg.Text(f'Disco libre: {Diskfree*0.0000000009313225746:.2f} GB')]]
 
 
 #Interfaz como tal
 
 layout = [[sg.TabGroup([[sg.Tab('CPU', tab1_layout), sg.Tab('RAM', tab2_layout), sg.Tab('GPU', tab3_layout), sg.Tab('Discos', tab4_layout)]])],
-              [sg.Button('Cerrar')]
+              [sg.Button('Actualizar')], [sg.Button('Cerrar')]
 
 ]
 
@@ -53,9 +71,19 @@ window = sg.Window('Monitoreo', layout)
 
 while True:
     event, values =window.read()
+    
+    Seconds = time.localtime().tm_sec
+
+    Timer = round(Seconds*0.1)
+    
     if event ==sg.WIN_CLOSED or event == 'Cerrar' :
         break
-    
+        
+#Intentando meter un temporizador en el bucle para actualizar los valores
+
+  """  elif Timer == 5 :
+        print('Hola') """
+        
 window.close(); del window
 
 #Intento de añadir un icon tray para poder minimizarla en segundo plano
