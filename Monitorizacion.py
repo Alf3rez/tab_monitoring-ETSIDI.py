2a16c4f383c86e8a74a3165860fa8470a8331787
Alfred0 <90577614+Alf3rez@users.noreply.github.com>
2021-09-17 11:32:24 +0200

    Update Monitorizacion.py

--- Monitorizacion.py
+++ Monitorizacion.py
@@ -11,6 +11,8 @@ import platform
 
 sg.theme('BrownBlue') #Para elegir el tema de la ventana
 
+# tiempo = 1 #Para establecer un tiempo inicial de 1 segundo cuando se abre el programa
+
     #Variables CPU
 
 CPUus = psutil.cpu_percent(interval=1)
@@ -42,48 +44,76 @@ Diskfree = psutil.disk_usage('/').free
 #Pestañas
 
 
-tab1_layout =  [[sg.Text(f'CPU carga: {CPUus} %')], 
-                [sg.Text(f'CPU frecuencia: {CPUfq*0.001} MHz')]
+tab1_layout =  [[sg.Text(f'CPU carga: {CPUus} %', key='CPUUS')], 
+                [sg.Text(f'CPU frecuencia: {CPUfq*0.001:.3f} MHz', key='CPUFEQ')]
                ]
 
-tab2_layout =  [[sg.Text(f'RAM carga: {RAMus} %')],
-                [sg.Text(f'RAM utilizada: {RAMused*0.0000000009313225746:.2f} GB')],
-                [sg.Text(f'RAM libre: {RAMfree*0.0000000009313225746:.2f} GB')]]
+tab2_layout =  [[sg.Text(f'RAM carga: {RAMus} %', key='RAMUS')],
+                [sg.Text(f'RAM utilizada: {RAMused*0.0000000009313225746:.2f} GB', key='RAMUSED')],
+                [sg.Text(f'RAM libre: {RAMfree*0.0000000009313225746:.2f} GB', key='RAMFREE')]]
                 
 tab3_layout =  [[sg.Text(f'GPU uso: {CPUus} %')], #Esta con los valores de la CPU para poder seguir usando el código sin tener que poner 4 o 5 comentarios mas mientras averiguo como sacar datos de la GPU
                 [sg.Text(f'GPU memoria: {CPUus} MB')]]
 
-tab4_layout =  [[sg.Text(f'Disco carga: {DISKus} %')],
-                [sg.Text(f'Disco utilizado: {Diskload*0.0000000009313225746:.2f} GB')],
-                [sg.Text(f'Disco libre: {Diskfree*0.0000000009313225746:.2f} GB')]]
+tab4_layout =  [[sg.Text(f'Disco carga: {DISKus} %', key='DISKUS')],
+                [sg.Text(f'Disco utilizado: {Diskload*0.0000000009313225746:.2f} GB', key='DISKLOAD')],
+                [sg.Text(f'Disco libre: {Diskfree*0.0000000009313225746:.2f} GB', key='DISKFREE')]]
 
 
 #Interfaz como tal
 
 layout = [[sg.TabGroup([[sg.Tab('CPU', tab1_layout), sg.Tab('RAM', tab2_layout), sg.Tab('GPU', tab3_layout), sg.Tab('Discos', tab4_layout)]])],
-              [sg.Button('Actualizar')], [sg.Button('Cerrar')]
+         # [sg.Text('Puedes ajustar el tiempo de actualización entre 1 segundo y 10 minutos')], [sg.InputText(key='IN')],
+         [sg.Button('Cerrar')]
 
 ]
+
+# tm = tiempo
 
 #Bucle de la ventana
 
 window = sg.Window('Monitoreo', layout)
 
 while True:
-    event, values =window.read()
-    
-    Seconds = time.localtime().tm_sec
-
-    Timer = round(Seconds*0.1)
+    event, values =window.read(timeout=1000)
     
     if event ==sg.WIN_CLOSED or event == 'Cerrar' :
         break
+    
+    #Actualizar tiempo
+    #sz_spin = int(values['spin'])
+    # if tm != tiempo:
+        # tiempo = tm
+        # window['IN'].update(tm)
         
-#Intentando meter un temporizador en el bucle para actualizar los valores
+    #Variables CPU
 
-  """  elif Timer == 5 :
-        print('Hola') """
-        
+    CPUus = psutil.cpu_percent(interval=1)
+    CPUfq = psutil.cpu_freq(percpu=False).current
+
+    #Variables RAM
+
+    RAMus = psutil.swap_memory().percent
+    RAMused = psutil.swap_memory().used
+    RAMfree = psutil.swap_memory().free
+    
+    #Actualizacion de CPU
+    
+    window['CPUUS'].update(f'CPU carga: {CPUus} %')
+    window['CPUFEQ'].update(f'CPU frecuencia: {CPUfq*0.001:.3f} MHz')
+    
+    #Actulizacion de RAM
+    
+    window['RAMUS'].update(f'RAM carga: {RAMus} %')
+    window['RAMUSED'].update(f'RAM utilizada: {RAMused*0.0000000009313225746:.2f} GB')
+    window['RAMFREE'].update(f'RAM libre: {RAMfree*0.0000000009313225746:.2f} GB')
+    
+    #Actualizacion de almacenamiento
+    
+    window['DISKUS'].update(f'Disco carga: {DISKus} %')
+    window['DISKLOAD'].update(f'Disco utilizado: {Diskload*0.0000000009313225746:.2f} GB')
+    window['DISKFREE'].update(f'Disco libre: {Diskfree*0.0000000009313225746:.2f} GB')
+
 window.close(); del window
 
 #Intento de añadir un icon tray para poder minimizarla en segundo plano
